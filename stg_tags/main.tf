37a0b23a31c934e3482f57203cc03949bae7ed18
Ben Hummerstone <bhummerstone@users.noreply.github.com>
2020-06-18 16:10:41 +0100

    fixed naming of storage account

--- stg_tags/main.tf
+++ stg_tags/main.tf
@@ -6,7 +6,7 @@ resource "random_string" "stg_suffix" {
 }
 
 resource "azurerm_storage_account" "stg_acct" {
-  name = "var.name${random_string.stg_suffix.result}"
+  name = "${var.name}${random_string.stg_suffix.result}"
   location = var.location
   resource_group_name = var.rg_name
   account_tier = var.type
